951605588310bb8d1a51069db6393724f075fac8
Luke Thompson <luke.thompson@xeonr.com>
2016-01-24 22:42:22 +0000

    Flarum has changed their API, which makes this a lot cleaner!

--- src/BeamAuthController.php
+++ src/BeamAuthController.php
@@ -1,87 +1,56 @@
 <?php
 namespace LukeT\BeamAuth;
 
-use Flarum\Forum\Controller\AuthenticateUserTrait;
-use Flarum\Forum\UrlGenerator;
-use Flarum\Http\Controller\ControllerInterface;
+use Flarum\Forum\AuthenticationResponseFactory;
+use Flarum\Forum\Controller\AbstractOAuth2Controller;
 use Flarum\Settings\SettingsRepositoryInterface;
-use Illuminate\Contracts\Bus\Dispatcher;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Zend\Diactoros\Response\RedirectResponse;
 
-
-class BeamAuthController implements ControllerInterface
+class BeamAuthController extends AbstractOAuth2Controller
 {
-    use AuthenticateUserTrait;
-
     /**
      * @var SettingsRepositoryInterface
      */
     protected $settings;
 
     /**
-     * @var UrlGenerator
-     */
-    protected $url;
-
-    /**
+     * @param AuthenticationResponseFactory $authResponse
      * @param SettingsRepositoryInterface $settings
-     * @param UrlGenerator $url
-     * @param Dispatcher $bus
      */
-    public function __construct(SettingsRepositoryInterface $settings, UrlGenerator $url, Dispatcher $bus)
+    public function __construct(AuthenticationResponseFactory $authResponse, SettingsRepositoryInterface $settings)
     {
         $this->settings = $settings;
-        $this->url = $url;
-        $this->bus = $bus;
+        $this->authResponse = $authResponse;
     }
 
-    /**
-     * @param Request $request
-     * @param array $routeParams
-     * @return \Psr\Http\Message\ResponseInterface|RedirectResponse
-     */
-    public function handle(Request $request, array $routeParams = [])
-    {
-        session_start();
 
-        $provider = new \League\OAuth2\Client\Provider\GenericProvider([
+    protected function getProvider($redirectUri) {
+        return new \League\OAuth2\Client\Provider\GenericProvider([
             'clientId'        => $this->settings->get('luket-beamauth.app_id'),
             'clientSecret'    => $this->settings->get('luket-beamauth.app_secret'),
-            'redirectUri'     => $this->url->toRoute('auth.beam'),
+            'redirectUri'     => $redirectUri,
             'urlAuthorize'    => "https://beam.pro/oauth/authorize",
             'urlAccessToken'  => "https://beam.pro/api/v1/oauth/token",
             'urlResourceOwnerDetails' => "https://beam.pro/api/v1/users/current"
         ]);
+    }
 
-        if (! isset($_GET['code'])) {
-            $authUrl = $provider->getAuthorizationUrl([
-                'scope' => ['user:details:self']
-            ]);
-            $_SESSION['oauth2state'] = $provider->getState();
+    protected function getAuthorizationUrlOptions()
+    {
+        return ['scope' => ['user:details:self']];
+    }
 
-            return new RedirectResponse($authUrl);
-        } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
-            unset($_SESSION['oauth2state']);
-            echo 'Invalid state.';
-            exit;
-        }
+    protected function getIdentification(ResourceOwnerInterface $resourceOwner)
+    {
+         return ['beam_id' => $resourceOwner->toArray()['id']];
+    }
 
-        $token = $provider->getAccessToken('authorization_code', [
-            'code' => $_GET['code']
-        ]);
-        
-
-
-        $owner = $provider->getResourceOwner($token);
-
-
-        $username = preg_replace('/[^a-z0-9-_]/i', '', $owner->toArray()['username']);
-        $email = $owner->toArray()['email'];
-        $id = $owner->toArray()['id'];
-        return $this->authenticate( 
-            ['beam_id' => $owner->toArray()['id']],
-            // ['email' => $owner->toArray()['email']]
-        );
+    protected function getSuggestions(ResourceOwnerInterface $resourceOwner)
+    {
+        return [
+            'username' => $resourceOwner->toArray()['username'],
+            'email' => $resourceOwner->toArray()['email']
+        ];
     }
 }
